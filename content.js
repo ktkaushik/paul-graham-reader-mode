8f8dc604cee69272edf4f2ce32605ff4a0e432fc
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2025-01-29 18:59:10 +0530

    Go to github

---
+++ content.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let table = document.querySelector("body > table");
+    if (table) {
+        table.style.display = "flex";
+        table.style.justifyContent = "center";
+    }
+});
